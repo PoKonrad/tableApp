15f647012cdc0bbe9c685034f03fad37ca572f35
Rad <rad@radski.eu>
2023-01-13 20:35:54 +0100

    added a fetcher

--- src/components/ProductsTable.tsx
+++ src/components/ProductsTable.tsx
@@ -6,67 +6,69 @@ import {
   TableCell,
   Paper,
   Typography,
+  TableFooter,
+  TablePagination,
+  TextField,
+  Alert,
+  AlertTitle,
+  LinearProgress,
+  Fade,
 } from "@mui/material";
-import React from "react";
-
-const data = {
-  page: 1,
-  per_page: 6,
-  total: 12,
-  total_pages: 2,
-  data: [
-    {
-      id: 1,
-      name: "cerulean",
-      year: 2000,
-      color: "#98B2D1",
-      pantone_value: "15-4020",
-    },
-    {
-      id: 2,
-      name: "fuchsia rose",
-      year: 2001,
-      color: "#C74375",
-      pantone_value: "17-2031",
-    },
-    {
-      id: 3,
-      name: "true red",
-      year: 2002,
-      color: "#BF1932",
-      pantone_value: "19-1664",
-    },
-    {
-      id: 4,
-      name: "aqua sky",
-      year: 2003,
-      color: "#7BC4C4",
-      pantone_value: "14-4811",
-    },
-    {
-      id: 5,
-      name: "tigerlily",
-      year: 2004,
-      color: "#E2583E",
-      pantone_value: "17-1456",
-    },
-    {
-      id: 6,
-      name: "blue turquoise",
-      year: 2005,
-      color: "#53B0AE",
-      pantone_value: "15-5217",
-    },
-  ],
-  support: {
-    url: "https://reqres.in/#support-heading",
-    text: "To keep ReqRes free, contributions towards server costs are appreciated!",
-  },
-};
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import TableItem from "./TableItem";
+import type { ApiResp } from "../types/apiResp";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const ProductsTable = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [idFilter, setIdFilter] = useState<string>();
+  let { current_page, id } = useParams();
+  let [searchParams, setSearchParams] = useSearchParams({
+    uwu: "uwu",
+    nice: "a",
+  });
+
+  const { data, error, isError, isLoading } = useQuery<ApiResp, Error>(
+    ["/products", currentPage, idFilter],
+    async (): Promise<ApiResp> => {
+      const params = new URLSearchParams({
+        per_page: "5",
+        page: currentPage + "1",
+      });
+
+      if (idFilter) {
+        params.append("id", idFilter);
+      }
+
+      const resp = await fetch(
+        `https://reqres.in/api/products?${params.toString()}`
+      );
+
+      if (!resp.ok) {
+        throw new Error(resp.status.toString());
+      }
+      const respJson = await resp.json();
+
+      return respJson;
+    }
+  );
   return (
     <TableContainer component={Paper}>
+      <Fade in={isLoading}>
+        <LinearProgress />
+      </Fade>
+      <Fade in={isError}>
+        <Alert severity="error">
+          <AlertTitle>An Error has occured</AlertTitle>
+          Error Code: {error?.message}
+        </Alert>
+      </Fade>
+      <TextField
+        type="number"
+        label="ID Filter"
+        onChange={(e) => setIdFilter(e.target.value)}
+      />
       <Table>
         <TableHead>
           <TableRow>
@@ -81,17 +83,23 @@ const ProductsTable = () => {
             </TableCell>
           </TableRow>
         </TableHead>
-        {data.data.map((el) => (
-          <TableRow
-            sx={{
-              backgroundColor: el.color,
+        {isLoading || isError ? null : Array.isArray(data?.data) ? (
+          data?.data.map((el) => <TableItem tableItem={el} key={el.id} />)
+        ) : (
+          <TableItem tableItem={data!.data} />
+        )}
+        <TableFooter>
+          <TablePagination
+            count={data?.total || 0}
+            page={currentPage}
+            rowsPerPage={data?.per_page || 0}
+            rowsPerPageOptions={[data?.per_page || 1]}
+            onPageChange={(e, newValue) => {
+              setCurrentPage(newValue);
+              console.log(newValue);
             }}
-          >
-            <TableCell>{el.id}</TableCell>
-            <TableCell>{el.name}</TableCell>
-            <TableCell>{el.year}</TableCell>
-          </TableRow>
-        ))}
+          />
+        </TableFooter>
       </Table>
     </TableContainer>
   );
